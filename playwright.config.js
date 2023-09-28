0d7bc998482755cca04b74bfbf5ad0e7e3dfd9d1
b_popovic <bojan.popovic@levi9.com>
2023-09-28 13:10:57 +0200

    playwright merge solution

--- playwright.config.js
+++ playwright.config.js
@@ -21,10 +21,16 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'], 
-    ['allure-playwright']
-  ],
+  reporter: process.env.CI
+  ? [
+      ['list'],
+      ['html', { open: 'never' }],
+      ['blob', { outputDir: 'playwright-blob-report' }]
+    ]
+  : [
+      ['list'],
+      ['html', { open: 'on-failure' }],
+    ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
