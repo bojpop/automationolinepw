a916d003d62925751f53565467269b72dab396e8
bojpop <bojpop@users.noreply.github.com>
2023-10-23 14:57:34 +0000

    deploy: a67523ccfe2b9942c5abb5dc9ece2026ecc4c165

--- .history/apis/AuthenticationApi_20230629121400.js
+++
@@ -1,9 +0,0 @@
-async loginApi(request, username, password){
-    const response = await request.post('/'+'auth/login/',{
-        data: {
-            username: username,
-            password: password
-        }
-    })
-    return response;
-}
